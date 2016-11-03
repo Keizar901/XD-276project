d5d354f7cfbd04e7bbccf77f3dfec78374fc43d8
James <james.abcde123@gmail.com>
2016-11-03 14:26:16 -0700

    added cookie for distance

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -7,23 +7,30 @@ class RandomController < ApplicationController
 
   def search
     client = Yelp::Client.new({consumer_key: 'FAWYz3fLKNkJk8y9hov-HQ',
-                            consumer_secret: 'IMSJ9Gtj-N4C7LiOeOOu8i3YVnk',
-                            token: '6gU0xJzU096r-ImUQXV5694Vj7RLipDk',
-                            token_secret: 's8SSdbNje6DN5Q4EfAtM7YgRye4'
-                          })
-    @distance = params[:distance]
-    lat_lng = cookies[:lat_lng].split("|")
+      consumer_secret: 'IMSJ9Gtj-N4C7LiOeOOu8i3YVnk',
+      token: '6gU0xJzU096r-ImUQXV5694Vj7RLipDk',
+      token_secret: 's8SSdbNje6DN5Q4EfAtM7YgRye4'
+      })
 
-    pref = { term: 'restaurants', radius_filter: @distance }
-    coordinates = { latitude: lat_lng[0], longitude: lat_lng[1] }
-    results = client.search_by_coordinates(coordinates, pref)
+      @distance = params[:distance]
+      if @distance
+        cookies[:distance] = @distance
+      else
+        @distance = cookies[:distance]
+      end
 
-    buses = results.businesses
-    index = Random.rand(0...buses.size)
-    bus = buses[index]
-    @name = bus.name
-    @img_url = bus.image_url
-    @img_placeholder = 'http://bit.ly/2fkkakw'
+      lat_lng = cookies[:lat_lng].split("|")
 
+      pref = { term: 'restaurants', radius_filter: @distance }
+      coordinates = { latitude: lat_lng[0], longitude: lat_lng[1] }
+      results = client.search_by_coordinates(coordinates, pref)
+
+      buses = results.businesses
+      index = Random.rand(0...buses.size)
+      bus = buses[index]
+      @name = bus.name
+      @img_url = bus.image_url
+      @img_placeholder = 'http://bit.ly/2fkkakw'
+
+    end
   end
-end
