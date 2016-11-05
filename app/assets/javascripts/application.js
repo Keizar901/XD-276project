a7d4d2bedcdd9ea2040ee6eb45ab07fa87064350
Andy <hiandyzhang@gmail.com>
2016-11-05 18:46:53 +0000

    add gmaps/google to the first line

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.min
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require underscore
-//= require gmaps/google
+
 
