1763957c400f59d1b9609a5833369a71f745943d
James <james.abcde123@gmail.com>
2016-11-16 23:59:43 -0800

    add create action for fb login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,6 +24,21 @@ class SessionsController < ApplicationController
     end
   end
 
+  def create_facebook
+    auth_hash = request.env['omniauth.auth']
+
+    @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+    if @authorization
+      render text: "Welcome back #{@authorization.user.name}! You have already signed up."
+    else
+      user = User.new name: auth_hash["info"]["name"], email: auth_hash["info"]["email"]
+      user.authorizations.build provider: auth_hash["provider"], uid: auth_hash["uid"]
+      user.save
+
+      render text: "Hi #{user.name}! You've signed up."
+    end
+  end
+
   def destroy
     log_out if logged_in?
     redirect_to root_url
