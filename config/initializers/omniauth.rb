b43f5061fa43eb11db4258b5639a0e940ff6cb39
James <james.abcde123@gmail.com>
2016-11-17 16:07:23 -0800

    add first_name and last_name to permission

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '803217126486136', 'fd8e2f5031ba9019c3b899e0d47affbb'
+  provider :facebook, '803217126486136', 'fd8e2f5031ba9019c3b899e0d47affbb', scope: 'email', info_fields: 'email, first_name, last_name'
 end
