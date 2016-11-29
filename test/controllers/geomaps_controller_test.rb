7a33404bf8421bb24329191cff5e78732451f5a8
Andy <hiandyzhang@gmail.com>
2016-11-28 21:02:45 -0800

    remove geomap controllewr

--- test/controllers/geomaps_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GeomapsControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get geomaps_index_url
-  #   assert_response :success
-  # end
-
-end
