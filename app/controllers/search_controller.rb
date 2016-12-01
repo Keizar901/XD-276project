b3c4a0c69e47c9654eec200a9deeba475efa7374
James <james.abcde123@gmail.com>
2016-12-01 15:31:13 -0800

    add search feature

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,12 @@
 class SearchController < ApplicationController
   def index
   end
+
+  def result
+    @prev_search_key = params[:search_key]
+    @results = User.where('lower(fname) = ?', @prev_search_key)
+    @results += User.where('lower(lname) = ?', @prev_search_key)
+    @results += User.where('lower(email) = ?', @prev_search_key)
+    @results.uniq! # remove ducplicates from the results it self
+  end
 end
