dcc01195ae2d23eb9c407190f1f20c7d4d81a42c
Keizar <keizar95@hotmail.com>
2016-12-01 23:41:56 -0800

    added review page

--- db/schema.rb
+++ db/schema.rb
@@ -78,13 +78,5 @@ ActiveRecord::Schema.define(version: 20161129220542) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
-  create_table "widgets", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "stock"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   add_foreign_key "reviews", "users"
 end
